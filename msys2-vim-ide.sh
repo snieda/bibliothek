ee8d6358628a5e36ef302221e20881c34bf2cd1f
TomS <snieda@web.de>
2018-12-09 14:29:02 +0100

    Update msys2-vim-ide.sh

--- msys2-vim-ide.sh
+++ msys2-vim-ide.sh
@@ -4,26 +4,26 @@
 # Then start msys2.exe and start this script!
 
 APT=pacman -S --noconfirm
-#APT=sudo apt install -Y
+#APT=sudo apt -y install
 
 echo "install system tools (~83MB)..."
-sudo $APT mc tree ytree htop git conky mupdf abiword antiword xclip fim cifs-utils  rar p7zip ntp xcompmgr tmux
+$INST mc tree ytree htop git conky mupdf abiword antiword xclip fim cifs-utils  rar p7zip ntp xcompmgr tmux
 
 echo "install console text tools..."
-sudo $APT vim ne dos2unix poppler-utils docx2txt catdoc colordiff icdiff colorized-logs kbtin pv bar
+$INST vim ne dos2unix poppler-utils docx2txt catdoc colordiff icdiff colorized-logs kbtin pv bar ripgrep
 
 echo "install networking tools..."
-sudo $APT nmap git curl wget openssh-server openvpn links2 w3m tightvncserver
+$INST nmap git curl wget openssh-server openvpn links2 w3m tightvncserver
 
 echo "vim plugin dependencies"
-sudo $APT make cmake gcc silversearcher-ag exuberant-ctags
+$INST make cmake gcc silversearcher-ag exuberant-ctags
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 curl https://raw.githubusercontent.com/snieda/bibliothek/master/install-linux-dev-vm.sh
 #wget https://github.com/ervandew/eclim/releases/download/2.8.0/eclim_2.8.0.bin
 #chmod a+x eclim_2.8.0.bin
 
 echo "python"
-$APT python3 python3-pip flake8
+$INST python3 python3-pip flake8
 pip install autopep8 pudb
 
 echo "installing all plugins for our vim-ide"
