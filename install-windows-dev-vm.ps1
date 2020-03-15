d34f5b95209acd8b5c8e1c345516c43a3a773175
TomS <snieda@web.de>
2020-03-15 15:04:17 +0100

    Update install-windows-dev-vm.ps1

--- install-windows-dev-vm.ps1
+++ install-windows-dev-vm.ps1
@@ -1,7 +1,7 @@
-# install script to provide a development system on windows
-# Thomas Schneider 03/2020
-# https://sourceforge.net/projects/gnuwin32/files/wget/1.11.4-1/wget-1.11.4-1-bin.zip/download
-# https://chocolatey.org/install
+REM install script to provide a development system on windows
+REM Thomas Schneider 03/2020
+REM https://sourceforge.net/projects/gnuwin32/files/wget/1.11.4-1/wget-1.11.4-1-bin.zip/download
+REM https://chocolatey.org/install
 
 # Set directory for installation - Chocolatey does not lock
 # down the directory if not the default
@@ -18,16 +18,15 @@ Set-ExecutionPolicy Bypass -Scope Process -Force;
 # [System.Net.WebRequest]::DefaultWebProxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-INST=choco install -y
-# choco upgrade chocolatey
+choco upgrade chocolatey
 
 # Tools
-$INST wget curl netcat openssh openvpn nmap tightvnc
-$INST fzf fd lf mc less bat 7zip vim googler ripgrep-all
-$INST gnupg
-$INST make
-$INST cmake.portable
-$INST msys2-installer
+choco install -y -dir  wget curl netcat openssh openvpn nmap tightvnc
+choco install -y -dir  fzf fd lf mc less bat 7zip vim googler ripgrep-all
+choco install -y -dir  gnupg
+choco install -y -dir  make
+choco install -y -dir  cmake.portable
+choco install -y -dir  msys2-installer
 curl https://dystroy.org/broot/download/x86_64-windows/broot -O ~/bin/broot
 wget -nc https://sourceforge.net/projects/squirrel-sql/files/latest/download/squirrel-sql-client.jar
 
@@ -35,46 +34,46 @@ wget https://www.pendrivelinux.com/downloads/YUMI/YUMI-2.0.6.9.exe
 wget https://www.pendrivelinux.com/downloads/YUMI/YUMI-UEFI-0.0.2.0.exe
 
 # Antivir
-$INST clamav
+choco install -y -dir  clamav
 
 # Editors
-#choco install visualstudiocode.portable -y # not yet available
-$INST git.commandline -y
-$INST notepadplusplus.commandline -y
-$INST vim-tux.portable
+#choco install -y -dir  visualstudiocode.portable -y # not yet available
+choco install -y -dir  git.commandline -y
+choco install -y -dir  notepadplusplus.commandline -y
+choco install -y -dir  vim-tux.portable
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 curl https://raw.githubusercontent.com/snieda/bibliothek/master/.vimrc > .vimrc
 vim +'PlugInstall --sync' +qa
 
 # Languages
-$INST jdk8 maven gradle
-$INST jdk11
-$INST anaconda3
-$INST activeperl
-$INST nodejs
-$INST python pip
+choco install -y -dir  jdk8 maven
+choco install -y -dir  jdk11
+choco install -y -dir  anaconda3
+choco install -y -dir  activeperl
+choco install -y -dir  nodejs
+choco install -y -dir  python pip
 
 pip install -U pip
 pip install flake8 autopep8 pudb
 
 # IDEs
-$INST eclipse-java-oxygen
-$INST vscode
+choco install -y -dir  eclipse-java-oxygen
+choco install -y -dir  vscode
 mkdir workspace
 echo "https://gist.github.com/snieda/0063c25f13cf8c9d5021941ca57ac895" > workspace/settings-sync-gist.txt 
 code --install-extension alphabotsec.vscode-eclipse-keybindings --install-extension  danields761.status-bar-breadcrumb --install-extension  dgileadi.java-decompiler --install-extension  donjayamanne.githistory --install-extension  donjayamanne.javadebugger --install-extension  DotJoshJohnson.xml --install-extension  eamodio.gitlens --install-extension  faustinoaq.javac-linter --install-extension  felipecaputo.git-project-manager --install-extension  IBM.XMLLanguageSupport --install-extension  masonicboom.web-browser --install-extension  ms-python.python --install-extension  ms-vsts.team --install-extension  msjsdiag.debugger-for-chrome --install-extension  qub.qub-xml-vscode --install-extension  redhat.java --install-extension  Shan.code-settings-sync --install-extension  VisualStudioExptTeam.vscodeintellicode --install-extension  vscjava.vscode-java-debug --install-extension  vscjava.vscode-java-dependency --install-extension  vscjava.vscode-java-pack --install-extension  vscjava.vscode-java-test --install-extension  vscjava.vscode-maven --install-extension  yzhang.markdown-all-in-one -a workspace
 
-$INST kitty, winscp.portable
-$INST winsshterm
+choco install -y -dir  kitty, winscp.portable
+choco install -y -dir  winsshterm
 
 # Standard Applications
-$INST opera
-$INST gimp
-$INST vlc
-$INST firefox
-$INST googlechrome
-$INST libreoffice-fresh
-$INST virtualbox
-$INST virtualbox-guest-additions-guest.install
-$INST citrix-workspace
+choco install -y -dir  opera
+choco install -y -dir  gimp
+choco install -y -dir  vlc
+choco install -y -dir  firefox
+choco install -y -dir  googlechrome
+choco install -y -dir  libreoffice-fresh
+choco install -y -dir  virtualbox
+choco install -y -dir  virtualbox-guest-additions-guest.install
+choco install -y -dir  citrix-workspace
