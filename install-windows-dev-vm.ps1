af6cf073f9d39c0f4c55908fa9526fdc3927f610
TomS <snieda@web.de>
2020-03-15 12:33:48 +0100

    Update install-windows-dev-vm.ps1

--- install-windows-dev-vm.ps1
+++ install-windows-dev-vm.ps1
@@ -18,15 +18,16 @@ Set-ExecutionPolicy Bypass -Scope Process -Force;
 # [System.Net.WebRequest]::DefaultWebProxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-choco upgrade chocolatey
+INST=choco install -y
+# choco upgrade chocolatey
 
 # Tools
-choco install -y wget curl netcat openssh openvpn nmap tightvnc
-choco install -y fzf fd lf mc less bat 7zip vim googler ripgrep-all
-choco install -y gnupg
-choco install -y make
-choco install -y cmake.portable
-choco install -y msys2-installer
+$INST wget curl netcat openssh openvpn nmap tightvnc
+$INST fzf fd lf mc less bat 7zip vim googler ripgrep-all
+$INST gnupg
+$INST make
+$INST cmake.portable
+$INST msys2-installer
 curl https://dystroy.org/broot/download/x86_64-windows/broot -O ~/bin/broot
 wget -nc https://sourceforge.net/projects/squirrel-sql/files/latest/download/squirrel-sql-client.jar
 
@@ -34,46 +35,46 @@ wget https://www.pendrivelinux.com/downloads/YUMI/YUMI-2.0.6.9.exe
 wget https://www.pendrivelinux.com/downloads/YUMI/YUMI-UEFI-0.0.2.0.exe
 
 # Antivir
-choco install clamav
+$INST clamav
 
 # Editors
 #choco install visualstudiocode.portable -y # not yet available
-choco install git.commandline -y
-choco install notepadplusplus.commandline -y
-choco install -y vim-tux.portable
+$INST git.commandline -y
+$INST notepadplusplus.commandline -y
+$INST vim-tux.portable
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 curl https://raw.githubusercontent.com/snieda/bibliothek/master/.vimrc > .vimrc
 vim +'PlugInstall --sync' +qa
 
 # Languages
-choco install -y jdk8 maven gradle
-choco install -y jdk11
-choco install -y anaconda3
-choco install -y activeperl
-choco install -y nodejs
-choco install -y python pip
+$INST jdk8 maven gradle
+$INST jdk11
+$INST anaconda3
+$INST activeperl
+$INST nodejs
+$INST python pip
 
 pip install -U pip
 pip install flake8 autopep8 pudb
 
 # IDEs
-choco install -y eclipse-java-oxygen
-choco install -y vscode
+$INST eclipse-java-oxygen
+$INST vscode
 mkdir workspace
 echo "https://gist.github.com/snieda/0063c25f13cf8c9d5021941ca57ac895" > workspace/settings-sync-gist.txt 
 code --install-extension alphabotsec.vscode-eclipse-keybindings --install-extension  danields761.status-bar-breadcrumb --install-extension  dgileadi.java-decompiler --install-extension  donjayamanne.githistory --install-extension  donjayamanne.javadebugger --install-extension  DotJoshJohnson.xml --install-extension  eamodio.gitlens --install-extension  faustinoaq.javac-linter --install-extension  felipecaputo.git-project-manager --install-extension  IBM.XMLLanguageSupport --install-extension  masonicboom.web-browser --install-extension  ms-python.python --install-extension  ms-vsts.team --install-extension  msjsdiag.debugger-for-chrome --install-extension  qub.qub-xml-vscode --install-extension  redhat.java --install-extension  Shan.code-settings-sync --install-extension  VisualStudioExptTeam.vscodeintellicode --install-extension  vscjava.vscode-java-debug --install-extension  vscjava.vscode-java-dependency --install-extension  vscjava.vscode-java-pack --install-extension  vscjava.vscode-java-test --install-extension  vscjava.vscode-maven --install-extension  yzhang.markdown-all-in-one -a workspace
 
-choco install -y kitty, winscp.portable
-choco install -y winsshterm
+$INST kitty, winscp.portable
+$INST winsshterm
 
 # Standard Applications
-choco install -y opera
-choco install -y gimp
-choco install -y vlc
-choco install -y firefox
-choco install -y googlechrome
-choco install -y libreoffice-fresh
-choco install -y virtualbox
-choco install -y virtualbox-guest-additions-guest.install
-choco install -y citrix-workspace
+$INST opera
+$INST gimp
+$INST vlc
+$INST firefox
+$INST googlechrome
+$INST libreoffice-fresh
+$INST virtualbox
+$INST virtualbox-guest-additions-guest.install
+$INST citrix-workspace
