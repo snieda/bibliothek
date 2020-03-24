664fb830387c7af88b70a8592b4fdf67633abda4
TomS <snieda@web.de>
2020-03-24 13:17:00 +0100

    Update install-win-scoop-dev.ps1

--- install-win-scoop-dev.ps1
+++ install-win-scoop-dev.ps1
@@ -31,22 +31,22 @@ wget https://www.pendrivelinux.com/downloads/YUMI/YUMI-2.0.6.9.exe
 wget https://www.pendrivelinux.com/downloads/YUMI/YUMI-UEFI-0.0.2.0.exe
 
 # install linux console
-scoop msys2
+scoop install msys2
 
 # install networking tools
 scoop install openssh openvpn gnupg netcat nmap-portable putty kitty winscp tightvnc
 
 # install languages
-scoop openjdk python
+scoop install openjdk python
 
 # install language additions
-scoop maven gradle nodejs rust anaconda3 pip-tool
+scoop install maven gradle nodejs rust anaconda3 pip-tool
 pip install -U pip
 pip install flake8 autopep8 pudb
 
 
 # install IDEs
-scoop vscode-portable eclipse-jee
+scoop install vscode-portable eclipse-jee
 mkdir workspace
 echo "https://gist.github.com/snieda/0063c25f13cf8c9d5021941ca57ac895" > workspace/settings-sync-gist.txt 
 code --install-extension alphabotsec.vscode-eclipse-keybindings --install-extension  danields761.status-bar-breadcrumb --install-extension  dgileadi.java-decompiler --install-extension  donjayamanne.githistory --install-extension  donjayamanne.javadebugger --install-extension  DotJoshJohnson.xml --install-extension  eamodio.gitlens --install-extension  faustinoaq.javac-linter --install-extension  felipecaputo.git-project-manager --install-extension  IBM.XMLLanguageSupport --install-extension  masonicboom.web-browser --install-extension  ms-python.python --install-extension  ms-vsts.team --install-extension  msjsdiag.debugger-for-chrome --install-extension  qub.qub-xml-vscode --install-extension  redhat.java --install-extension  Shan.code-settings-sync --install-extension  VisualStudioExptTeam.vscodeintellicode --install-extension  vscjava.vscode-java-debug --install-extension  vscjava.vscode-java-dependency --install-extension  vscjava.vscode-java-pack --install-extension  vscjava.vscode-java-test --install-extension  vscjava.vscode-maven --install-extension  yzhang.markdown-all-in-one -a workspace
